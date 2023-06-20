881684f66bb35ad891a6f7af9648602955ef19de
Samuel de Alencar Rocha <samuel.lizardo@ngosolucoes.com.br>
2023-06-20 19:57:45 -0300

    wip: deciding wich logo will be the one

--- src/client/components/header/index.js
+++ src/client/components/header/index.js
@@ -3,7 +3,15 @@ import "./index.css";
 export default function Header() {
   return (
     <header className="header">
-      <img className="logo_img" src="t.png" />
+      <div className="logo_div">
+        <img className="logo_img" src="talky_cropped.png" />
+      </div>
+
+      <section className="headerButtons_section">
+        <button className="menu_button">
+          <img className="menuIcon_img" src="menu_black.png" />
+        </button>
+      </section>
     </header>
   );
 }
